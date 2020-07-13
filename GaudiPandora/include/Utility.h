e75c1f9a13a436ee84030b4ff826b821b214dc72
Fang Wenxing <wxfang@lxslc601.ihep.ac.cn>
2020-07-13 11:40:00 +0800

    update

--- GaudiPandora/include/Utility.h
+++
@@ -1,6 +0,0 @@
-#ifndef MYUTILITY
-#define MYUTILITY 1
-
-#include <sstream>
-std::string Convert (float number);
-#endif
--- GaudiPandora/src/Utility.cpp
+++
@@ -1,7 +0,0 @@
-#include "Utility.h"
-
-std::string Convert (float number){
-    std::ostringstream buff;
-    buff<<number;
-    return buff.str();   
-}
